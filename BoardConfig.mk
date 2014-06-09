a1ade1a02ebc719f9e23829fae9e0c6b1ebdcb8f
Mkey_NJU <499238317@qq.com>
2014-06-09 23:23:09 +0800

    first commit

---
+++ BoardConfig.mk
@@ -0,0 +1,158 @@
+#
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from the proprietary version
+-include vendor/zte/n909/BoardConfigVendor.mk
+
+USE_CAMERA_STUB := true
+
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+BOARD_WANTS_EMMC_BOOT := true
+
+TARGET_SPECIFIC_HEADER_PATH := device/zte/n909/include
+
+# Architecture and CPU
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a5
+TARGET_BOARD_PLATFORM := msm7x27a
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno203
+ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_CPU_SMP := true
+TARGET_BOOTLOADER_BOARD_NAME := n909
+
+# Qualcomm hardware
+BOARD_USES_QCOM_HARDWARE := true
+COMMON_GLOBAL_CFLAGS += -DQCOM_BSP -DQCOM_HARDWARE
+TARGET_USES_ION := true
+TARGET_USES_QCOM_BSP := true
+
+# Compiler flags
+TARGET_GLOBAL_CFLAGS += -mfloat-abi=softfp -mfpu=neon -mtune=cortex-a5
+TARGET_GLOBAL_CPPFLAGS += -mfloat-abi=softfp -mfpu=neon -mtune=cortex-a5
+
+# Audio
+TARGET_HAS_QACT := true
+BOARD_USES_LEGACY_ALSA_AUDIO := true
+TARGET_QCOM_AUDIO_VARIANT := legacy
+TARGET_PROVIDES_LIBAUDIO := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/zte/n909/bluetooth
+
+# Camera
+COMMON_GLOBAL_CFLAGS += -DMR0_CAMERA_BLOB -DNEEDS_VECTORIMPL_SYMBOLS
+
+# GPS
+BOARD_USES_QCOM_GPS := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
+
+# Graphics
+BOARD_ADRENO_DECIDE_TEXTURE_TARGET := true
+BOARD_EGL_CFG := device/zte/n909/configs/egl.cfg
+BOARD_EGL_WORKAROUND_BUG_10194508 := true
+BOARD_USE_MHEAP_SCREENSHOT := true
+TARGET_DOESNT_USE_FENCE_SYNC := true
+TARGET_QCOM_DISPLAY_VARIANT := legacy
+USE_OPENGL_RENDERER := true
+
+# Hardware
+BOARD_HARDWARE_CLASS := device/zte/n909/cmhw
+
+# Kernel
+BOARD_KERNEL_BASE := 0x00200000
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.selinux=permissive
+BOARD_PAGE_SIZE := 2048
+TARGET_KERNEL_SOURCE := kernel/zte/n909
+TARGET_KERNEL_CONFIG := n909_defconfig
+
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
+
+# Media
+TARGET_QCOM_MEDIA_VARIANT := legacy
+TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+
+# FM
+BOARD_HAVE_QCOM_FM := true
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x800000
+BOARD_CACHEIMAGE_PARTITION_SIZE := 201326592 # 0xC000000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x1400000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824 # 0x40000000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1207943168 # 0x47FFC000
+BOARD_FLASH_BLOCK_SIZE := 131072
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# Recovery
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/zte/n909/recovery/recovery-keys.c
+BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
+TARGET_RECOVERY_INITRC := device/zte/n909/recovery/init.rc
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+DEVICE_RESOLUTION := 480x854
+TARGET_RECOVERY_FSTAB := device/zte/n909/rootdir/fstab.qcom
+BOARD_CUSTOM_GRAPHICS := ../../../device/zte/n909/recovery/graphics.c
+
+# CWM_CN
+#BOARD_CUSTOM_GRAPHICS := ../../../device/zte/n909/recovery/graphics_cn.c
+#BOARD_USE_CUSTOM_RECOVERY_FONT := \"fontcn22_12x32.h\"
+
+# CM Recovery
+#RECOVERY_VARIANT := cm
+
+
+# RIL
+BOARD_RIL_CLASS := ../../../device/zte/n909/ril/
+
+# SELinux
+BOARD_SEPOLICY_DIRS += device/zte/n909/sepolicy
+BOARD_SEPOLICY_UNION += \
+    file_contexts \
+    file.te
+
+# USB
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+BOARD_VOLD_MAX_PARTITIONS := 19
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/msm_hsusb/gadget/lun%d/file"
+TARGET_USE_CUSTOM_SECOND_LUN_NUM := 1
+
+# Web Rendering
+ENABLE_WEBGL := true
+PRODUCT_PREBUILT_WEBVIEWCHROMIUM := yes
+TARGET_FORCE_CPU_UPLOAD := true
+
+# Wi-Fi
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_HOSTAPD_DRIVER := NL80211
+TARGET_CUSTOM_WIFI := ../../device/zte/n909/libhardware_legacy/wifi/wifi.c
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_HAS_ATH_WLAN := true
+BOARD_WLAN_DEVICE := ath6kl
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_FW_PATH_AP := "ap"
+WIFI_DRIVER_FW_PATH_STA := "sta"
+WIFI_DRIVER_FW_PATH_P2P := "p2p"
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/ath6kl/ath6kl_sdio.ko"
+WIFI_DRIVER_MODULE_NAME := "wlan"
+WIFI_EXT_MODULE_PATH := "/system/lib/modules/ath6kl/cfg80211.ko"
+WIFI_EXT_MODULE_NAME := "cfg80211"
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+sh device/zte/n909/patches/apply.sh
+add_lunch_combo cm_n909-userdebug
